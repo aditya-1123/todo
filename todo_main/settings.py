49df2b11df32961a97abb50df43d030070324707
aditya-1123 <gaikwadaditya1123@gmail.com>
2024-06-27 16:49:41 +0530

    done vercel setting

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-ee6%n1@_w$pom8=f7!ip0q$9#ar9^5m%m!djx53yzp0)c2&@tp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 # Application definition
 
@@ -116,9 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
